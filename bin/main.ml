5f000091b6e96e4379acf87bff3ee569b670d441
Petru Eric Stavarache <estavarache@janestreet.com>
2019-08-13 14:02:47 +0100

    Counter can now handle multiple files

--- bin/main.ml
+++ bin/main.ml
@@ -11,27 +11,30 @@ let print_block (block : Cfg.block) =
   printf "}\n"
 ;;
 
-let main file =
+let main files =
   let open Linear_format in
-  let linear_item_info = read file in
-  Cmm.set_label linear_item_info.last_label;
-  let items =
-    List.filter_map linear_item_info.items ~f:(function
-      | Func d ->
-          d.decl
-          |> Cfg_builder.from_linear ~preserve_orig_labels:false
-          |> Some
-      | Data _ -> None)
-  in
   let equivalence_counter =
-    List.fold items ~init:Equivalence_class.empty ~f:(fun acc cfg_builder ->
-        let layout = Cfg_builder.get_layout cfg_builder in
-        let blocks =
-          List.map layout ~f:(fun label ->
-              Cfg_builder.get_block cfg_builder label |> Option.value_exn)
+    List.fold files ~init:Equivalence_class.empty
+      ~f:(fun equivalence_counter file ->
+        let linear_item_info = read file in
+        Cmm.set_label linear_item_info.last_label;
+        let items =
+          List.filter_map linear_item_info.items ~f:(function
+            | Func d ->
+                d.decl
+                |> Cfg_builder.from_linear ~preserve_orig_labels:false
+                |> Some
+            | Data _ -> None)
         in
-        (* List.iter blocks ~f:print_block; *)
-        List.fold ~init:acc blocks ~f:Equivalence_class.update)
+        List.fold items ~init:equivalence_counter ~f:(fun acc cfg_builder ->
+            let layout = Cfg_builder.get_layout cfg_builder in
+            let blocks =
+              List.map layout ~f:(fun label ->
+                  Cfg_builder.get_block cfg_builder label
+                  |> Option.value_exn)
+            in
+            (* List.iter blocks ~f:print_block; *)
+            List.fold ~init:acc blocks ~f:Equivalence_class.update))
   in
   let equivalence_list = Equivalence_class.to_alist equivalence_counter in
   List.take equivalence_list 10
@@ -51,8 +54,8 @@ let main_command =
       "Group contents of basic blocks based modulo register renaming, and \
        print the most common quotients.")
     [%map_open.Command.Let_syntax
-      let file = anon ("input" %: Filename.arg_type) in
-      fun () -> main file]
+      let files = anon (sequence ("input" %: Filename.arg_type)) in
+      fun () -> main files]
 ;;
 
 let () = Command.run main_command
