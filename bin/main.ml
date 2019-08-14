254758a5205a5ab0d42fc0f2b8aeffc3b682291f
Petru Eric Stavarache <estavarache@janestreet.com>
2019-08-14 16:42:06 +0100

    Add more filtering options

--- bin/main.ml
+++ bin/main.ml
@@ -2,8 +2,16 @@ open Core
 open Ocamlcfg
 open Instr_freq
 
+let block_size_for_equivalence_class equivalence_counter equivalence_class =
+  Equivalence_class.representative_blocks equivalence_counter
+    equivalence_class
+  |> Option.value_exn |> List.hd_exn
+  |> fun block ->
+  1 + List.length block.Cfg.body
+;;
+
 let main files ~representatives_per_equivalence_class ~most_frequent_classes
-    ~block_print_mode =
+    ~block_print_mode ~min_block_size ~min_equivalence_class_size =
   let open Linear_format in
   let equivalence_counter =
     List.fold files
@@ -31,23 +39,38 @@ let main files ~representatives_per_equivalence_class ~most_frequent_classes
             (* List.iter blocks ~f:print_block; *)
             List.fold ~init:acc blocks ~f:Equivalence_class.update))
   in
-  let equivalence_list = Equivalence_class.to_alist equivalence_counter in
+  let equivalence_list =
+    Equivalence_class.to_alist equivalence_counter
+    |> List.filter ~f:(fun (equivalence_class, _number_of_members) ->
+           block_size_for_equivalence_class equivalence_counter
+             equivalence_class
+           >= min_block_size)
+    |> List.filter ~f:(fun (_equivalence_class, number_of_members) ->
+           number_of_members >= min_equivalence_class_size)
+  in
   List.take equivalence_list most_frequent_classes
-  |> List.iter ~f:(fun (key, data) ->
-         printf "for equivalence class %d we have %d entries\n" key data;
+  |> List.iter ~f:(fun (equivalence_class, number_of_members) ->
+         printf "for equivalence class %d we have %d entries\n"
+           equivalence_class number_of_members;
          printf "Representative blocks: \n";
-         Equivalence_class.representative_blocks equivalence_counter key
+         Equivalence_class.representative_blocks equivalence_counter
+           equivalence_class
          |> Option.value_exn
          |> List.iter ~f:(Utils.print_block ~block_print_mode);
          printf "\n========================================\n");
 
-  List.iter equivalence_list ~f:(fun (key, data) ->
-      Equivalence_class.representative_blocks equivalence_counter key
-      |> Option.value_exn |> List.hd_exn
-      |> (fun block -> 1 + List.length block.Cfg.body)
-      |> printf
-           !"for equivalence class %d we have %d entries; block sizes are %d\n"
-           key data)
+  let equivalence_classes_of_each_size = Hashtbl.create (module Int) in
+  List.iter equivalence_list
+    ~f:(fun (_equivalence_class, number_of_members) ->
+      Hashtbl.update equivalence_classes_of_each_size number_of_members
+        ~f:(function
+        | Some x -> x + 1
+        | None -> 1));
+  printf
+    !"(numbers of members in equivalence class, how many equivalence \
+      classes there are with this many members):\n\
+      %{sexp: (int, int) Hashtbl.t}\n"
+    equivalence_classes_of_each_size
 ;;
 
 let main_command =
@@ -68,6 +91,18 @@ let main_command =
       and print_blocks_as_assembly =
         flag "-print-block-as-assembly" no_arg
           ~doc:"Print blocks as assembly instead of cfg format"
+      and min_block_size =
+        flag "-min-block-size"
+          (optional_with_default 5 int)
+          ~doc:
+            "n Only report equivalence classes, for which the \
+             representative block has at least [n] instructions."
+      and min_equivalence_class_size =
+        flag "-min-equivalence-class-size"
+          (optional_with_default 5 int)
+          ~doc:
+            "n Only report equivalence classes which have at least [n] \
+             members"
       in
       let block_print_mode =
         match print_blocks_as_assembly with
@@ -76,7 +111,8 @@ let main_command =
       in
       fun () ->
         main files ~representatives_per_equivalence_class
-          ~most_frequent_classes ~block_print_mode]
+          ~most_frequent_classes ~block_print_mode ~min_block_size
+          ~min_equivalence_class_size]
 ;;
 
 let () = Command.run main_command
