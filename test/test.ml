a75f74863a93c8f375d915859451aa58f1dbe97b
Petru Eric Stavarache <estavarache@janestreet.com>
2019-09-03 17:26:27 +0100

    Add test for matcher across basic blocks

--- test/test.ml
+++ test/test.ml
@@ -15,16 +15,18 @@ let find_fixtures_directory () =
   walk (Filename.realpath Sys.executable_name)
 ;;
 
-let matcher_for_move index =
-  Index.Matcher.create
-    [ {
-        Index.With_register_information.desc =
-          Index.Matcher.Basic (Types.From_cfg.Op Types.From_cfg.Move);
-        arg = [| 0 |> Equivalence.Register_equivalence.of_int |];
-        res = [| 1 |> Equivalence.Register_equivalence.of_int |];
-      }
-    ]
-    index
+let matcher_of_sexp sexp =
+  let fname = Filename.temp_file "matcher" ".sexp" in
+  Out_channel.write_all fname ~data:sexp;
+  let matcher =
+    let instructions =
+      Sexp.load_sexps_conv_exn fname
+        [%of_sexp: Index.Matcher.desc Index.With_register_information.t]
+    in
+    Index.Matcher.create instructions
+  in
+  Sys.remove fname;
+  matcher
 ;;
 
 let build_index_for_fixture ~fixtures_directory ~context_length fixture_name
@@ -85,7 +87,16 @@ let test_matching_functionality ~fixtures_directory =
   let blocks, index =
     build_index_for_fixture "simple" ~fixtures_directory ~context_length:0
   in
-  let matcher = Some (matcher_for_move index) in
+  let matcher =
+    let sexp = {|
+(
+ (desc (Basic (Op Move)))
+ (arg (0))
+ (res (1))
+)
+|} in
+    Some (matcher_of_sexp sexp index)
+  in
   let statistics =
     Stats.combine
       [ Stats.count_blocks_matching index ~min_block_size:0 ~matcher;
@@ -132,11 +143,53 @@ let test_index_context ~fixtures_directory =
   iter_blocks blocks ~index ~file:"repetitive.ml" ~statistics
 ;;
 
+let test_index_context_with_matcher ~fixtures_directory =
+  printf "test_index_context_with_matcher\n";
+  let blocks, index =
+    build_index_for_fixture "long_function" ~fixtures_directory
+      ~context_length:2
+  in
+  let matcher =
+    let sexp =
+      {|
+(
+ (desc (Basic (Op (Intop_imm Iadd -2))))
+ (arg (0))
+ (res (0))
+)
+(
+ (desc (Terminator
+        (Branch
+         (((Test (Iinttest (Isigned Cgt))) 101)
+          ((Test (Iinttest (Isigned Cle))) 127)))))
+ (arg (1 0))
+ (res ())
+)
+(
+ (desc (Basic (Op Spill)))
+ (arg (0))
+ (res (2))
+)
+|}
+    in
+    Some (matcher_of_sexp sexp index)
+  in
+  let statistics =
+    Stats.combine
+      [ Stats.print_most_popular_classes index ~n_real_blocks_to_print:1
+          ~n_most_frequent_equivalences:1
+          ~block_print_mode:Loop_free_block.Both ~min_block_size:0 ~matcher
+      ]
+  in
+  iter_blocks blocks ~index ~file:"repetitive.ml" ~statistics
+;;
+
 let main ~fixtures_directory =
   test_index_statistics ~fixtures_directory;
   test_matching_functionality ~fixtures_directory;
   test_index_bigger_file ~fixtures_directory;
-  test_index_context ~fixtures_directory
+  test_index_context ~fixtures_directory;
+  test_index_context_with_matcher ~fixtures_directory
 ;;
 
 let () = main ~fixtures_directory:(find_fixtures_directory ())
